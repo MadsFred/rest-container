890dd9e609639af3d20fd8ad91d5e593cd145d74
washer <madspelt@newsio.com>
2016-09-27 19:42:06 +0200

    fix data stuff

--- rest-container/index.js
+++ rest-container/index.js
@@ -10,19 +10,24 @@ const optionDefinitions = [
 ]
 
 const OPTIONS     = commandLineArgs(optionDefinitions)
-const PORT        = OPTIONS.port_number || 3000; //6554;
-// const PYTHON_VER  = OPTIONS.python_version || 'python';
+const PORT        = OPTIONS.port_number || 3000;
 const SCRAPE_PAGE = OPTIONS.scrape_page || 'http://datawactest.com/AlexandraInstituttet_output.php'
 
 var server = restify.createServer({
   name: 'rest-container',
+
 });
 
 server.get('containers', function(req, res, next) {
 
     scraper.get(SCRAPE_PAGE).then(function(tableData){
-        var json_data = JSON.stringify(tableData);
-        res.json(json_data);
+        var json_data = tableData[0];
+
+        if(!json_data) {
+            res.send('No data found :(');
+        }
+
+        res.json(tableData[0]);
 
     }).catch(function (e){
         res.send(e)
